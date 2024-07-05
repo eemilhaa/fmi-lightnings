6338d3aecb67c4a876c8eb362a564d4d8e491f5f
eemilhaa <eemil.haapanen@gmail.com>
2024-07-05 16:58:04 +0300

    initial working version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fmi-lightnings
-a dump of lightning data from fmi
+
+Geodata on lightning strikes.
+
+## Data:
+- Source: Finnish Meteorological Institute
+- License: Creative Commons Attribution 4.0 International license
+  - https://creativecommons.org/licenses/by/4.0/
---
+++ download.py
@@ -0,0 +1,75 @@
+from fmiopendata.lightning import Lightning
+import datetime
+import csv
+from fmiopendata.wfs import download_stored_query
+
+
+def main():
+    init_csv()
+    start_date = datetime.date(2024, 6, 1)
+    end_date = datetime.date(2024, 7, 1)
+    dates = split_to_weeks(start_date, end_date)
+    arg_list = format_args(dates)
+    for args in arg_list:
+        print(args)
+        obs = download(args=args)
+        write_obs(obs)
+
+
+def split_to_weeks(start_date: datetime.date, end_date: datetime.date):
+    dates = []
+    date = start_date
+    addition = datetime.timedelta(7)
+    while date + addition < end_date:
+        date = date + addition
+        dates.append(date)
+    dates.append(end_date)
+    return dates
+
+
+def format_args(dates: list[datetime.date]):
+    arg_list = []
+    for date in dates:
+        start_date = date - datetime.timedelta(days=7)
+        end_date = date - datetime.timedelta(days=1)
+        args = [
+            f"starttime={start_date}T00:00:00Z",
+            f"endtime={end_date}T23:59:59Z",
+        ]
+        arg_list.append(args)
+
+    return arg_list
+
+
+def download(args) -> Lightning:
+    lightnings = download_stored_query(
+        "fmi::observations::lightning::multipointcoverage",
+        args=args,
+    )
+    return lightnings
+
+
+def init_csv():
+    with open("lightnings.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        header = ["time", "peak_current", "latitude", "longitude"]
+        writer.writerow(header)
+
+
+def write_obs(obs):
+    zipped_obs = zip(obs.times, obs.peak_current, obs.latitudes, obs.longitudes)
+
+    with open("lightnings.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        for time, peak_current, lat, lon in zipped_obs:
+            row = [
+                int(time.timestamp()),
+                abs(peak_current),
+                lat,
+                lon,
+            ]
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    main()
